36e0ffb1b5f072f9136dadc9b33501553452a6dd
miguel angel <miguelangelguaiv@gmail.com>
2024-01-11 20:07:32 +0100

    add: ex2 string

--- data-types/string.js
+++ data-types/string.js
@@ -9,3 +9,17 @@ let upper = document.getElementById("upper");
 upper.textContent=toUpper;
 
 //2. Check for spam
+function checkSpam(str){
+
+    if((str.toLowerCase().includes('viagra'))||(str.toLowerCase().includes('xxx'))) return true
+
+    return false;
+}
+let checkSpa=[checkSpam('buy ViAgRA now'),
+checkSpam('free xxxxx'),
+checkSpam('innocent rabbit')];
+
+let spam = document.getElementById("spam");
+spam.textContent=checkSpa;
+
+//3. Truncate the text
