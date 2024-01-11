78bbbfbbee61a0119149c9a9768d0a34a963631d
miguel angel <miguelangelguaiv@gmail.com>
2024-01-11 19:53:11 +0100

    add: ex1 in arrays

--- data-types/string.js
+++ data-types/string.js
@@ -1,2 +1,11 @@
-let par = document.getElementById("a");
-par.textContent="Imbécil";
+//1. uppercase the first character
+function ucFirst(word) {
+    if (!word)return word;
+    return word[0].toUpperCase()+word.slice(1);
+}
+let toUpper=ucFirst("john");//John
+
+let upper = document.getElementById("upper");
+upper.textContent=toUpper;
+
+//2. Check for spam
