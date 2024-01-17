5d16123c35159947097fde380d312e508cc13681
miguel angel <miguelangelguaiv@gmail.com>
2024-01-17 16:38:52 +0100

    add:ex3 number

--- data-types/number.js
+++ data-types/number.js
@@ -8,8 +8,26 @@ let a = document.getElementById("sumNumbers");
 a.textContent=sumNums;
 
 //2. Why 6.35.toFixed(1)==6.3?
-
 let b1 = document.getElementById("toFixedA");
 b1.textContent=1.35.toFixed(1);
 let b2 = document.getElementById("toFixedB");
 b2.textContent=6.35.toFixed(1);
+
+//3. Repeat until the input is a number
+function readNumber(){
+    let input;
+    do{
+        input = prompt("Please write a number");
+    }while(!isFinite(input));
+
+    if (input===""||input===null){
+        return null;
+    } else return input;
+    
+
+}
+let repeatInput = readNumber();
+
+let c = document.getElementById("repeatInput");
+c.textContent=repeatInput;
+
