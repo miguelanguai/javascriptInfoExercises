fc60dd37775f5129fd55aeafe5804dc1fbdc994b
miguel angel <miguelangelguaiv@gmail.com>
2024-01-18 19:03:44 +0100

    add: ex2 object-methods

--- objects/object-methods.js
+++ objects/object-methods.js
@@ -10,3 +10,23 @@ user = makeUser();
 
 a = document.getElementById("usingThis");
 a.textContent=JSON.stringify(user);
+
+//2. Create a calculator
+let calculator={
+    a,
+    b,
+    read(){
+        this.a=prompt("Input first value");
+        this.b=prompt("Input second value");
+    },
+    sum(){
+        return this.a+this.b;
+    },
+    mul(){
+        return this.a*this.b;
+    }
+}
+calculator.read();
+
+b = document.getElementById("usingThis");
+b.textContent=JSON.stringify(calculator);
