a580f729f0e54d432920815d3bb0ff20de000f03
miguel angel <miguelangelguaiv@gmail.com>
2024-01-18 18:21:26 +0100

    ex2 objects

--- objects/objects.js
+++ objects/objects.js
@@ -9,12 +9,21 @@ delete user.name;
 let a = document.getElementById("helloObject");
 a.textContent=user.toString();
 
-//1. Check for emptiness
-let user = {};
-user.name="John";
-user.surname="Smith";
-user.name="Pete";
-delete user.name;
+//2. Check for emptiness
+function isEmpty(obj){
+    for (let prop in obj){
+        return false;
+    }
+    return true;
+}
+let schedule={};
+let noProperties = isEmpty(schedule);
+
+let b = document.getElementById("checkEmptiness");
+b.textContent=noProperties;
+
+//3. Sum Object Properties
+
 
 let a = document.getElementById("helloObject");
 a.textContent=user.toString();
